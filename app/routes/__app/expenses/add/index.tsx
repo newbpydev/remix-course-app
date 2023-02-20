3816c9b73a00ed13f4a20b5a35f8b8be4839c14b
Juan Gomez <94386361+newbpydev@users.noreply.github.com>
2023-02-20 13:50:03 -0300

    fix validate data structure, add action to formdat

--- app/routes/__app/expenses/add/index.tsx
+++ app/routes/__app/expenses/add/index.tsx
@@ -1,8 +1,14 @@
+import { ActionFunction, redirect } from "@remix-run/node";
 import { useNavigate } from "@remix-run/react";
 import ExpenseForm from "~/components/expenses/ExpenseForm";
 import Modal from "~/components/util/Modal";
+import { addExpense } from "~/data/expenses.server";
 
-function AddExpensePage() {
+async function formGetter(form: FormData, inputName: string) {
+  return (await form).get(inputName)?.toString();
+}
+
+export default function AddExpensePage() {
   const navigate = useNavigate();
 
   function closeHandler() {
@@ -18,4 +24,19 @@ function AddExpensePage() {
   );
 }
 
-export default AddExpensePage;
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData();
+  // const expenseData = Object.fromEntries(formData);
+
+  const expenseData = {
+    title: await formGetter(formData, "title"),
+    amount: await formGetter(formData, "amount"),
+    date: await formGetter(formData, "date"),
+
+    // title: (await formData).get("title")?.toString(),
+    // amount: (await formData).get('amount')?.toString(),
+  };
+
+  await addExpense(expenseData);
+  return redirect("/expenses");
+};
